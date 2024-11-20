625a0a1f41725caf49283a82c7e249034789c441
Rohan Thacker <thackerrohan4@gmail.com>
2024-11-20 14:09:33 +0530

    WIP: Added AzureAICreateArgs

--- python/packages/autogen-ext/src/autogen_ext/models/_azure/config/__init__.py
+++ python/packages/autogen-ext/src/autogen_ext/models/_azure/config/__init__.py
@@ -1,12 +1,36 @@
-from typing import TypedDict, Required, Union
+from typing import TypedDict, Required, Union, Optional, List, Dict, Any
 
 from azure.core.credentials import AzureKeyCredential
 from azure.core.credentials_async import AsyncTokenCredential
+from azure.ai.inference.models import (
+    ChatCompletionsResponseFormat,
+    ChatCompletionsToolDefinition,
+    ChatCompletionsToolChoicePreset,
+    ChatCompletionsNamedToolChoice,
+)
 
 from autogen_core.components.models import ModelCapabilities
 
 
-class AzureAIChatCompletionClientConfig(TypedDict, total=False):
+GITHUB_MODELS_ENDPOINT = "https://models.inference.ai.azure.com"
+
+
+class AzureAICreateArgs(TypedDict, total=False):
+    frequency_penalty: Optional[float]
+    presence_penalty: Optional[float]
+    temperature: Optional[float]
+    top_p: Optional[float]
+    max_tokens: Optional[int]
+    response_format: Optional[ChatCompletionsResponseFormat]
+    stop: Optional[List[str]]
+    tools: Optional[List[ChatCompletionsToolDefinition]]
+    tool_choice: Optional[Union[str, ChatCompletionsToolChoicePreset, ChatCompletionsNamedToolChoice]]
+    seed: Optional[int]
+    model: Optional[str]
+    model_extras: Optional[Dict[str, Any]]
+
+
+class AzureAIChatCompletionClientConfig(AzureAICreateArgs, total=False):
     endpoint: Required[str]
-    credentials: Required[Union[AzureKeyCredential | AsyncTokenCredential]]
+    credential: Required[Union[AzureKeyCredential | AsyncTokenCredential]]
     model_capabilities: Required[ModelCapabilities]
